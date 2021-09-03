825f4bad522d0248322c429a5e1e037eb7f9d73c
Bruno Martins <bruno.martins@outsystems.com>
2021-09-03 11:17:17 +0100

    Typed JS Testing

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+/* Typed JS Scripts */
+var typed = new Typed(".typing", {
+    strings: ["Writing ideias...","Experimenting...","Designing...", "Coding..."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
